66ffb74f55f42613879488eb91c2df6a17fc5826
Seonghun <zoseonghun@gmail.com>
2025-06-27 12:06:41 +0900

    Query를 이용한 검색 처리

--- src/main/java/com/example/shop/repository/ItemRepository.java
+++ src/main/java/com/example/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.example.shop.repository;
 
 import com.example.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +16,9 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like " +
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
 }
