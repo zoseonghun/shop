037d760becd7c35617a85ce16f58fd06c56b9826
Seonghun <zoseonghun@gmail.com>
2025-06-27 11:54:38 +0900

    상품 조회하기 테스트

--- src/main/java/com/example/shop/repository/ItemRepository.java
+++ src/main/java/com/example/shop/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package com.example.shop.repository;
 import com.example.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
 }
--- src/test/java/com/example/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -31,6 +32,29 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    public void createItemList(){
+        for(int i=1;i<=10;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100); item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
     }
 
 }
