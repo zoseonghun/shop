eebf6b8599868b7ce14e07406701727d31516642
Seonghun <zoseonghun@gmail.com>
2025-06-28 08:59:36 +0900

    Cart 엔티티 작성

---
+++ src/main/java/com/shop/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.shop.repository;
+
+import com.shop.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+}
