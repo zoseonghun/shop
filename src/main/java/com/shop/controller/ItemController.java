539381d890cfe1dfcfb0068bb4419c17d6225e21
Seonghun <zoseonghun@gmail.com>
2025-10-24 15:21:23 +0900

    상품 수정 - 등록된 페이지 진입, 등록한 상품 확인

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,5 +1,6 @@
 package com.shop.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +9,7 @@ import com.shop.dto.ItemFormDto;
 
 import com.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
@@ -23,31 +25,49 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping(value = "/admin/item/new")
-    public String itemForm(Model model){
+    public String itemForm(Model model) {
         model.addAttribute("itemFormDto", new ItemFormDto());
         return "item/itemForm";
     }
 
     @PostMapping(value = "/admin/item/new")
     public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
-                          Model model, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList){
+                          Model model, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList) {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "item/itemForm";
         }
 
-        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null){
+        if (itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
             model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
             return "item/itemForm";
         }
 
         try {
             itemService.saveItem(itemFormDto, itemImgFileList);
-        } catch (Exception e){
+        } catch (Exception e) {
             model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
             return "item/itemForm";
         }
 
         return "redirect:/";
     }
+
+    @GetMapping(value = "/admin/item/{itemId}")
+    public String itemDtl(@PathVariable("itemId") Long itemId, Model model) {
+
+        try {
+            // 조회한 상품 데이터를 모델에 담아서 뷰로 전달
+            ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+            model.addAttribute("itemFormDto", itemFormDto);
+            // 상품 엔티티가 존재하지 않을 경우 에러메시지를 담아서 상품 등록 페이지로 이동
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("errorMessage", "존재하지 않는 상품입니다.");
+            model.addAttribute("itemFormDto", new ItemFormDto());
+            return "item/itemForm";
+        }
+
+        return "item/itemForm";
+    }
+
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,16 +1,20 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -45,5 +49,25 @@ public class ItemService {
         return item.getId();
     }
 
+    // 상품 데이터를 읽어오는 트랜잭션을 읽기 전용으로 설정
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId) {
+
+        // 해당 상품의 이미지를 조회(등록순으로 가지고 오기 위해 상품 이미지 아이디 오름차순으로 가지고 옴)
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        // 조회한 ItemImg 엔티티를 ItemImgDto 객체로 만들어서 리스트에 추가
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        // 상품의 아이디를 통해 상품 엔티티를 조회(존재하지 않을 때 EntityNotFoundException 발생)
+        Item item = (Item) itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+
+    }
 
 }
