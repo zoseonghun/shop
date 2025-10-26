f53bca8c9e3486262bfc3e441eb0aea871967b74
Seonghun <zoseonghun@gmail.com>
2025-10-26 13:20:33 +0900

    메인 화면 - 구현

--- src/main/java/com/shop/config/WebMvcConfig.java
+++ src/main/java/com/shop/config/WebMvcConfig.java
@@ -9,12 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${uploadPath}")
-    String uploadPath;
+    private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**")
+
+        registry.addResourceHandler("/images/item/**")
                 .addResourceLocations(uploadPath);
     }
 
 }
+
