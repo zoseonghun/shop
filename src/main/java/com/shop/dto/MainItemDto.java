eaf9c726d16ba051b039490215564c4ab23963be
Seonghun <zoseonghun@gmail.com>
2025-10-26 10:11:49 +0900

    메인 화면 - MainItemDto 생성

---
+++ src/main/java/com/shop/dto/MainItemDto.java
@@ -0,0 +1,30 @@
+package com.shop.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MainItemDto {
+
+    private Long id;
+
+    private String itemNm;
+
+    private String itemDetail;
+
+    private String imgUrl;
+
+    private Integer price;
+
+    // 생성자에 @QueryProjection 어노테이션을 선언하여 Querydsl로 결과 조회 시 MainItemDto 객체로 바로 받아오도록 함
+    @QueryProjection
+    public MainItemDto(Long id, String itemNm, String itemDetail, String imgUrl, Integer price) {
+        this.id = id;
+        this.itemNm = itemNm;
+        this.itemDetail = itemDetail;
+        this.imgUrl = imgUrl;
+        this.price = price;
+    }
+
+}
