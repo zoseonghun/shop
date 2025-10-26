b15685b3056ce9a7f3556bc7958969133474bba7
Seonghun <zoseonghun@gmail.com>
2025-10-26 10:16:28 +0900

    메인 화면 - ItemRepositoryCustom에 상품 리스트를 가져오는 메서드 작성

--- src/main/java/com/shop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.shop.repository;
 
 import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 public interface ItemRepositoryCustom {
 
     // 상품 조회 조건을 담고 있는데 itemSearchDto 객체와 페이징 정보를 담고 있는 pageable 객체를 파라미터로 받는 getAdminItemPage 메서드 정의 (반환 데이터로 Page<Item> 객체를 반환)
-    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+    Page <Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    Page <MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 
 }
