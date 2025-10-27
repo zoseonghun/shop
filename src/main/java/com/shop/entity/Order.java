81539224554ef4d76c3cb0650ba07cb486af8d37
Seonghun <zoseonghun@gmail.com>
2025-10-27 13:08:03 +0900

    주문 기능 - 주문 객체를 만드는 메서드 추가

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
-public class Order {
+public class Order extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "order_id")
@@ -32,5 +32,37 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    // 주문 상품 정보들을 담아줌
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        // Order 엔티티와 OrderItem 엔티티가 양방향 참조 관계라 orderItem 객체에도 order 객체를 세팅
+        orderItem.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, List<OrderItem> orderItemList) {
+
+        Order order = new Order();
+        // 상품을 주문한 회원의 정보를 세팅
+        order.setMember(member);
+        // 여러 개의 주문 상품을 담을 수 있도록 리스트형태로 파라미터 값을 받으며 주문 객체에 orderItem 객체를 추가
+        for (OrderItem orderItem : orderItemList) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);    // 주문 상태를 ORDER로 세팅
+        order.setOrderDate(LocalDateTime.now());    // 현재 시간을 주문 시간으로 세팅
+        return order;
+
+    }
+
+    // 총 주문 금액을 구하는 메서드
+    public int getTotalPrice() {
+
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+
+    }
 
 }
