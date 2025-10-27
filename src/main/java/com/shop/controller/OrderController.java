2a3f04ce451048aff0fd7ee32462709b6fe0f845
Seonghun <zoseonghun@gmail.com>
2025-10-27 14:21:25 +0900

    주문 기능 - 주문 관리 요청처리 위해 OrderController 추가

---
+++ src/main/java/com/shop/controller/OrderController.java
@@ -0,0 +1,57 @@
+package com.shop.controller;
+
+import com.shop.dto.OrderDto;
+import com.shop.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    // 비동기 처리 : @ResponseBody, @RequestBody 어노테이션 사용
+    // @RequestBody : HTTP 요청의 본물 body에 담긴 내용을 자바 객체로 전달
+    // @ResponseBody : 자바 객체를 HTTP 요청의 body로 전달
+    @PostMapping(value = "/order")
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto, BindingResult bindingResult, Principal principal) {
+
+        // 주문 정보를 받는 orderDto 객체에 데이터 바인딩 시 에러가 있는지 검사
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            List <FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            // 에러 정보를 ResponseEntity 객체에 담아서 반환
+            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        // principal 객체에서 현재 로그인한 회원의 이메일 정보를 조회
+        String email = principal.getName();
+        Long orderId;
+
+        try {
+            // 넘어오는 주문 정보와 회원의 이메일 정보를 이용하여 주문 로직을 호출
+            orderId = orderService.order(orderDto, email);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+
+    }
+
+}
