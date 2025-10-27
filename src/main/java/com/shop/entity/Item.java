c6dde200720cf3b06ac3d629d34c76cd6f99ad0f
Seonghun <zoseonghun@gmail.com>
2025-10-27 11:36:39 +0900

    주문 기능 - 상품을 주문할 경우 상품의 재고를 감소시키는 로직 추가

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -2,6 +2,7 @@ package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
 import com.shop.dto.ItemFormDto;
+import com.shop.exception.OutOfStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -56,6 +57,17 @@ public class Item extends BaseEntity {
         this.stockNumber = itemFormDto.getStockNumber();
         this.itemDetail = itemFormDto.getItemDetail();
         this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+
+    public void removeStock(int stockNumber) {
+        // 상품의 재고 수량에서 주문 후 남은 재고 수량을 구함
+        int restStock = this.stockNumber - stockNumber;
+        // 상품의 재고가 주문 수량보다 작을 경우 재고 부족 예외 발생
+        if (restStock < 0) {
+            throw new OutOfStockException("상품의 재고가 부족합니다. 현재 재고 수량 : " + this.stockNumber);
+        }
+        // 주문 후 남은 재고 수량을 상품의 현재 재고 값으로 할당
+        this.stockNumber = restStock;
     }
 
 }
