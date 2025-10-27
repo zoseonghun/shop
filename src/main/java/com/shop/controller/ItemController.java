247a218e57dcc60ace21a1d464d5843969041d12
Seonghun <zoseonghun@gmail.com>
2025-10-27 10:10:20 +0900

    상품 상세 페이지 - 상세 페이지 이동할수 있도록 ItemController 코드 추가

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -61,14 +61,14 @@ public class ItemController {
     }
 
     @GetMapping(value = "/admin/item/{itemId}")
-    public String itemDtl(@PathVariable("itemId") Long itemId, Model model){
+    public String itemDtl(@PathVariable("itemId") Long itemId, Model model) {
 
         try {
             // 조회한 상품 데이터를 모델에 담아서 뷰로 전달
             ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
             model.addAttribute("itemFormDto", itemFormDto);
             // 상품 엔티티가 존재하지 않을 경우 에러메시지를 담아서 상품 등록 페이지로 이동
-        } catch(EntityNotFoundException e){
+        } catch (EntityNotFoundException e) {
             model.addAttribute("errorMessage", "존재하지 않는 상품 입니다.");
             model.addAttribute("itemFormDto", new ItemFormDto());
             return "item/itemForm";
@@ -79,20 +79,20 @@ public class ItemController {
 
     @PostMapping(value = "/admin/item/{itemId}")
     public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
-                             @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model){
+                             @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model) {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "item/itemForm";
         }
 
-        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null){
+        if (itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
             model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
             return "item/itemForm";
         }
 
         try {
             itemService.updateItem(itemFormDto, itemImgFileList);
-        } catch (Exception e){
+        } catch (Exception e) {
             model.addAttribute("errorMessage", "상품 수정 중 에러가 발생하였습니다.");
             return "item/itemForm";
         }
@@ -108,7 +108,7 @@ public class ItemController {
         // 첫 번째 파라미터로 조회 : 페이지 번호, 두 번째 파라미터 : 한 번에 가지고 올 데이터 수, 페이지 번호가 있으면 해당 페이지 조회, 없으면 0페이지 조회
         Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
         // 조회 조건과 페이징 정보를 파라미터로 넘겨서 Page(Item) 객체를 반환받음
-        Page <Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
         // 조회한 상품 데이터 및 페이징 정보를 뷰에 전달
         model.addAttribute("items", items);
         // 페이지 전환 시 기존 검색 조건을 유지한 채 이동할 수 있도록 뷰에 다시 전달
@@ -119,7 +119,12 @@ public class ItemController {
 
     }
 
+    @GetMapping(value = "/item/{itemId}")
+    public String itemDtl(Model model, @PathVariable("itemId") Long itemId) {
 
+        ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+        model.addAttribute("item", itemFormDto);
+        return "item/itemDtl";
 
-
+    }
 }
