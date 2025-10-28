f64b5a1db902e2e4a2803add30d11bdb08911d8f
Seonghun <zoseonghun@gmail.com>
2025-10-28 13:01:46 +0900

    주문 이력 조회 - ItemImgReposiotry에 상품의 대표 이미지를 찾는 쿼리 메서드 추가

--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -9,4 +9,6 @@ public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 
+    ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
+
 }
