a86d86e3ee6127adf60976144ecf1d3ffa45ca47
Seonghun <zoseonghun@gmail.com>
2025-10-28 14:01:18 +0900

    주문 이력 조회 - OrderService 주문 목록을 조회하는 로직 구현

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -1,15 +1,18 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
-import com.shop.entity.Item;
-import com.shop.entity.Member;
-import com.shop.entity.Order;
-import com.shop.entity.OrderItem;
+import com.shop.dto.OrderHistDto;
+import com.shop.dto.OrderItemDto;
+import com.shop.entity.*;
+import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +28,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         // 주문을 상품 조회
@@ -33,7 +37,7 @@ public class OrderService {
         // 현재 로그인한 회원의 이메일을 정보를 이용해서 회원 정보 조회
         Member member = memberRepository.findByEmail(email);
 
-        List < OrderItem> orderItemList = new ArrayList<>();
+        List<OrderItem> orderItemList = new ArrayList<>();
         // 주문할 상품 엔티티와 주문 수량을 이용하여 주문 상품 엔티티 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount());
         orderItemList.add(orderItem);
@@ -46,4 +50,34 @@ public class OrderService {
         return order.getId();
 
     }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+
+        // 1. 유저의 아이디와 페이징 조건을 이용하여 주문 목록을 조회
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+
+        // 2. 유저의 주문 총 개수를 구함
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        // 3. 주문 리스트를 순회하면서 구매 이력 페이지에 전달할 DTO 생성
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                // 4. 주문한 상품의 대표 이미지를 조회
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+            orderHistDtos.add(orderHistDto);
+        }
+
+        // 5. 페이지 구현 객체를 생성하여 반환
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
+
+    }
+
 }
