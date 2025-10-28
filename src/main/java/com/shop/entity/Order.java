f2f6e75514ca4177b532c7252e8ae47bf4fcea47
Seonghun <zoseonghun@gmail.com>
2025-10-28 21:40:35 +0900

    주문 취소하기 - Item클래스에 주문 취소 시 주문 수량을 상품의 재고에 더해주는 로직, 주문 상태를 취소 상태로 바꿔주는 메서드 구현

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -65,4 +65,14 @@ public class Order extends BaseEntity {
 
     }
 
+    public void cancelOrder() {
+
+        this.orderStatus = OrderStatus.CANCEL;
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+
+    }
+
 }
