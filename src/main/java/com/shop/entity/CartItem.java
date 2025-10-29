7be94d90c19f650ccca64c4f2ba5fc66c3e680cf
Seonghun <zoseonghun@gmail.com>
2025-10-29 11:46:54 +0900

    장바구니 - 장바구니에 담을 상품 엔티티를 생성하는 메서드, 장바구니에 담을 수량을 증가시켜주는 메서드를 CartItem클래스에 추가

--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -8,7 +8,7 @@ import lombok.ToString;
 @Entity
 @Getter @Setter
 @Table(name = "cart_item")
-public class CartItem {
+public class CartItem extends BaseEntity{
 
     @Id
     @GeneratedValue
@@ -24,4 +24,20 @@ public class CartItem {
     private Item item;
 
     private int count;
+
+    public static CartItem createCartItem(Cart cart, Item item, int count) {
+
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+        return cartItem;
+
+    }
+
+    // 장바구니에 기존에 담겨 있는 상품인데, 해당 상품을 추가로 장바구니에 담을 때 기존 수량에 현재 담을 수량을 더 해줄때 사용할 메서드
+    public void addCount(int count) {
+        this.count += count;
+    }
+
 }
