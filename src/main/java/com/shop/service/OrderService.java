301d6f3b2c98db239916d8915d893a783e88b9e5
Seonghun <zoseonghun@gmail.com>
2025-10-30 15:28:17 +0900

    장바구니 상품 주문하기 - OrderService클래스에 장바구니에서 주문할 상품 데이터를 전달받아서 주문을 생성하는 로직 추가

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -103,6 +103,27 @@ public class OrderService {
         order.cancelOrder();
     }
 
+    public Long orders(List<OrderDto> orderDtoList, String email) {
+
+        Member member = memberRepository.findByEmail(email);
+        List<OrderItem> orderItemList = new ArrayList<>();
+
+        // 주문할 상품 리스트를 만듬
+        for (OrderDto orderDto : orderDtoList) {
+            Item item = itemRepository.findById(orderDto.getItemId()).orElseThrow(EntityNotFoundException::new);
+            OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount());
+            orderItemList.add(orderItem);
+        }
+
+        // 현재 로그인한 회원과 주문 상품 목록을 이용하여 주문 엔티티를 만듬
+        Order order = Order.createOrder(member, orderItemList);
+        // 주문 데이터를 저장
+        orderRepository.save(order);
+
+        return order.getId();
+
+    }
+
 
 
 }
